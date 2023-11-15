ea603acba7e6edc1f67e0e33a13faf1e8dedc253
your_name <nomanwahid123@gmail.com>
2023-11-15 21:11:21 +0500

    fix: some bug

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-import todoSlice from "./slices/todoSlice";
-import alertSlice from "./slices/alertSlice";
-import themeSlice from "./slices/themeSlice";
+import todoSlice from "./slices/todoslice.js";
+import alertSlice from "./slices/alertSlice.js";
+import themeSlice from "./slices/themeslice.js";
 
 export default configureStore({
   reducer: {
